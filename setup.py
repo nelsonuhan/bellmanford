80f7ce7f7005a4819d5f61d6ff309db32eaddeee
Nelson Uhan <nelson@uhan.me>
2017-01-12 17:02:23 -0500

    Convert README in setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+import setuptools
+import pypandoc
 
-setup(
+long_description = pypandoc.convert('README.md', 'rst')
+
+setuptools.setup(
     name='bellmanford',
     version='0.1',
     description='''
         Small extensions of the Bellman-Ford routines in NetworkX, primarily
         for convenience (https://networkx.github.io).
     ''',
+    long_description=long_description,
     url='https://github.com/nelsonuhan/bellmanford',
     author='Nelson Uhan',
     author_email='nelson@uhan.me',
