4e52e75521037d300453fbf944d7bf20997bbd84
Nelson Uhan <nelson@uhan.me>
2017-01-12 15:40:06 -0500

    Better description for setuptools

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='bellmanford',
     version='0.1',
     description='''
-        Convenience wrappers for the Bellman-Ford routines in NetworkX
-        (https://networkx.github.io).
+        Small extensions of the Bellman-Ford routines in NetworkX, primarily
+        for convenience (https://networkx.github.io).
     ''',
     url='https://github.com/nelsonuhan/bellmanford',
     author='Nelson Uhan',
