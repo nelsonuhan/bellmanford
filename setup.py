6fe03e86fcfc7774f7fd30baaea127886d4f636b
Nelson Uhan <nelson@uhan.me>
2018-05-07 15:36:03 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setuptools.setup(
     name='bellmanford',
-    version='0.2',
+    version='0.2.1',
     description='''
         Small extensions of the Bellman-Ford routines in NetworkX, primarily
         for convenience (https://networkx.github.io).
